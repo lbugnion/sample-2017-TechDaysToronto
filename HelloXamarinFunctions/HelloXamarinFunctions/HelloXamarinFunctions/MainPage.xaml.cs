268e1fa1ddf173979ee419f74c6c41f13896c0f8
Lbugnion <lbugnion@outlook.com>
2017-12-09 23:10:41 +0100

    Initialized with HelloXamarin and HelloXamarinFunctions

---
+++ HelloXamarinFunctions/HelloXamarinFunctions/HelloXamarinFunctions/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using Xamarin.Forms;
+
+namespace HelloXamarinFunctions
+{
+    public partial class MainPage : ContentPage
+	{
+        private const string Url = "https://lbxamarindemo.azurewebsites.net/api/HttpTriggerCSharp/name/{name}?code=h6kv9xW9b/m7rWu40ioBl32k2HxhMldJaUPu1jbP62FanaD5mQXNOA==";
+
+        public MainPage()
+		{
+			InitializeComponent();
+
+            GoButton.Clicked += async (s, e) =>
+            {
+                ResultText.Text = "Please wait";
+
+                var client = new HttpClient();
+
+                var result = await client.GetStringAsync(
+                    Url.Replace("{name}", FirstNameText.Text));
+
+                ResultText.Text = result;
+            };
+		}
+	}
+}
